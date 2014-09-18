90e9a746fced30a367c4c47595d0ed355922e310
Yume <yume190@gmail.com>
2014-09-18 18:37:09 +0800

    Update unittest_01.py

--- unittest_01.py
+++ unittest_01.py
@@ -1,7 +1,7 @@
 class RomanNumeralConverter(object):
     def __init__(self, roman_numeral):
         self.roman_numeral = roman_numeral
-        self.digit_map = {"M":1000, "D":500, "C":100, "L":50, "X":100, "V":5, "I":1}
+        self.digit_map = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
 
     def convert_to_decimal(self):
         val = 0
